a698744e061884c7654764c179d2907ebad32c89
cayanezduron <adrianyanez0614@gmail.com>
2025-05-05 14:19:40 -0600

    Update Satellite.java

--- Satellite.java
+++ Satellite.java
@@ -1,5 +1,24 @@
+/**
+ * Represents a satellite object in orbit.
+ * Inherits from {@code SpaceObject}.
+ */
 public class Satellite extends SpaceObject {
 
+/**
+* Constructs a {@code Satellite} object with the specified attributes.
+*
+* @param recordId          the record ID of the object
+* @param satelliteName     the name of the satellite
+* @param country           the country of origin
+* @param orbitType         the orbit type (e.g., LEO, MEO, GEO)
+* @param launchYear        the year the object was launched
+* @param launchSite        the launch site
+* @param longitude         the current longitude
+* @param avgLongitude      the average longitude
+* @param geohash           the geohash representing the location
+* @param daysOld           number of days since launch
+* @param conjunctionCount  number of recorded conjunction events
+*/
     public Satellite(String recordId, String satelliteName, String country, String orbitType,
                      int launchYear, String launchSite, double longitude, double avgLongitude,
                      String geohash, int daysOld, int conjunctionCount) {
