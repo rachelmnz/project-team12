c60abac7831fbb6c92c1fd155b9e2a9ec80b36a6
cayanezduron <adrianyanez0614@gmail.com>
2025-05-05 15:04:12 -0600

    Update UnknownObject.java

--- UnknownObject.java
+++ UnknownObject.java
@@ -1,4 +1,24 @@
+/**
+ * Represents a space object with an unknown classification.
+ * Inherits properties from {@code SpaceObject} and sets the type to "UNKNOWN".
+ */
 public class UnknownObject extends SpaceObject {
+
+ /**
+* Constructs an {@code UnknownObject} with the specified attributes.
+*
+* @param recordId          unique record ID
+* @param satelliteName     name of the object
+* @param country           country of origin
+* @param orbitType         type of orbit (e.g., LEO, GEO)
+* @param launchYear        year the object was launched
+* @param launchSite        launch site location
+* @param longitude         current longitude of the object
+* @param avgLongitude      average longitude over time
+* @param geohash           geohash location code
+* @param daysOld           number of days since launch
+* @param conjunctionCount  number of conjunction events
+*/
 
     public UnknownObject(String recordId, String satelliteName, String country, String orbitType,
                          int launchYear, String launchSite, double longitude, double avgLongitude,
